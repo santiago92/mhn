f3c1a9a82124756085f6e1f9019a7348f2f2eead
santiago92 <santi.alasia92@gmail.com>
2016-06-11 14:49:47 -0300

    actualización a flask 11

--- server/mhn.py
+++ server/mhn.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-
 if __name__ == '__main__':
     from mhn import mhn
-    mhn.run(debug=False, host='0.0.0.0', port=8000)
+    mhn.run(debug=True, host='0.0.0.0', port=8080)
+
